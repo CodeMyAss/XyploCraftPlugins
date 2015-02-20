13d2f6dcb763d626ebf2fdfc85003afa818ef54b
LoonyRules <H4YD0N.P@live.co.uk>
2015-02-20 06:49:49 +0000

    Updates

--- Bano/src/xyplocraft/net/bano/permissions/PermissionCommands.java
+++ Bano/src/xyplocraft/net/bano/permissions/PermissionCommands.java
@@ -26,6 +26,7 @@ public class PermissionCommands implements CommandExecutor
 					Player p = Bukkit.getPlayer(args[1]);
 					if(args[1] != null && p != null)
 					{
+						
 						if(args[2].equalsIgnoreCase("add"))
 						{
 							if((args[3] != null) && !(args[3].contains(" ")))
@@ -37,9 +38,39 @@ public class PermissionCommands implements CommandExecutor
 								} else sender.sendMessage(ChatColor.RED + "You don't have permission to give a player a permission.");
 							}
 						}
+						
+						if(args[2].equalsIgnoreCase("remove"))
+						{
+							if((args[3] != null) && !(args[3].contains(" ")))
+							{
+								if(pUtils.hasPermission(sender, "perm.user.remove"))
+								{
+									pUtils.removePlayerPermission(p.getUniqueId(), args[3]);
+									sender.sendMessage(ChatColor.GREEN + "The permission " + ChatColor.YELLOW + args[3] + ChatColor.GREEN + " was set for " + ChatColor.YELLOW + args[1] + ChatColor.GREEN + ".");
+								} else sender.sendMessage(ChatColor.RED + "You don't have permission to remove a players permission.");
+							}
+						} 
+						
+						if(args[2].equalsIgnoreCase("check"))
+						{
+							if((args[3] != null) && !(args[3].contains(" ")))
+							{
+								if(pUtils.hasPermission(sender, "perm.user.check"))
+								{
+									String status;
+									if(pUtils.hasPermission(p, args[3]))
+									{
+										status = ChatColor.GREEN + " does";
+									} else status = ChatColor.RED + " doesn't";
+									sender.sendMessage(ChatColor.YELLOW + args[1] + status + ChatColor.GREEN + " have the permission: " + ChatColor.YELLOW + args[3]);
+								} else sender.sendMessage(ChatColor.RED + "You don't have permission to check a playes permission.");
+							}
+						} 
+						
 					} // erm... not sure yet
 				}
 			}
+			return true;
 		}
 		return false;
 	}
